b93c1608a67c7c50a163e6aa91fbdfb3d0edc8bb
ophari <69293070+ophari@users.noreply.github.com>
2021-11-27 19:31:44 +0700

    Add files via upload

---
+++ Windows GUI/GUI 1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<windows.h>
+
+int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+{
+	MessageBox(NULL,"HELLO","my FIRST GUI",MB_OK);
+	return 0;
+}
+

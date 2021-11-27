8860f096a148a37d1dbe99a15a4a83c58121ccb0
ophari <69293070+ophari@users.noreply.github.com>
2021-11-27 19:32:17 +0700

    Delete Windows GUI.cpp

--- Windows GUI.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<windows.h>
-
-int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
-{
-	MessageBox(NULL,"HELLO","my FIRST GUI",MB_OK);
-	return 0;
-}
-
